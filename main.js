5b16f57543d932ecbbda84f3342eedf0d88f226b
jaeyunim00 <ijy00120877@gmail.com>
2023-02-15 22:52:52 +0900

    add time, few html

--- main.js
+++ main.js
@@ -1,10 +1,24 @@
-const id_date = document.getElementById("date");
+const current_date = document.getElementById("current_date");
+const current_time = document.getElementById("current_time");
 
-const date = new Date();
+function getDate() {
+  // date obj
+  const date = new Date();
 
-const year = date.getFullYear();
-const month = date.getMonth();
-const day = date.getDate();
+  // date
+  const year = date.getFullYear();
+  const month = date.getMonth();
+  const day = date.getDate();
 
-document.getElementById("current_date").innerHTML =
-  year + "/" + month + "/" + day;
+  // time
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+
+  //html code
+  current_date.innerHTML = year + "/" + month + "/" + day;
+  current_time.innerHTML = hours + ":" + minutes + ":" + seconds;
+}
+
+getDate();
+setInterval(getDate, 1000);
