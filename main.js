73a41bfaa7342301b2a1629d6189b78a7c4eb5d1
jaeyunim00 <ijy00120877@gmail.com>
2023-02-24 23:01:43 +0900

    add login function, css

--- main.js
+++ main.js
@@ -1,33 +1,64 @@
+//CONST TIME
 const current_date = document.getElementById("current_date");
 const current_time = document.getElementById("current_time");
 
-const user_name = document.querySelector(".user_name");
-const user_name_input = user_name.querySelector("input");
+//CONST USER
+const login = document.querySelector(".login");
+const login_form = login.querySelector("form");
+const login_form_name = login_form.querySelector(".login_name");
+const login_greet = login.querySelector("#greet");
 
+//CONST
+const NAME_KEY = "username";
+
+//TIME
 function getDate() {
-  // date obj
   const date = new Date();
 
-  // date
   const year = date.getFullYear();
   const month = date.getMonth();
   const day = date.getDate();
 
-  // time
   const hours = date.getHours();
   const minutes = date.getMinutes();
   const seconds = date.getSeconds();
 
-  //html code
   current_date.innerHTML = year + "/" + month + "/" + day;
   current_time.innerHTML = hours + ":" + minutes + ":" + seconds;
 }
-
 getDate();
 setInterval(getDate, 1000);
 
-function onLogin() {
-  console.log(user_name_input.value);
+//LOGIN
+
+function myFunction() {
+  login_form.classList.add("magictime", "openDownRightOut");
+  login.classList.add("magictime", "openDownRightOut");
+  login_form_name.classList.add("magictime", "openDownRightOut");
+  login_greet.classList.add("magictime", "openDownRightOut");
 }
 
-user_name_input.addEventListener("submit", onLogin());
+function handleLogin(event) {
+  event.preventDefault();
+  const username = login_form_name.value;
+  localStorage.setItem(NAME_KEY, username);
+  login_form.setAttribute("id", "hidden");
+
+  login_greet.innerHTML = `Hello ${username}`;
+  login_greet.setAttribute("id", "");
+}
+
+const storageUser = localStorage.getItem(NAME_KEY);
+
+if (storageUser === null) {
+  login_form.setAttribute("id", "");
+  login_greet.setAttribute("id", "hidden");
+} else {
+  login_greet.innerHTML = `Hello ${storageUser}`;
+  login_greet.setAttribute("id", "");
+  login_form.setAttribute("id", "hidden");
+}
+
+console.log(storageUser);
+
+login_form.addEventListener("submit", handleLogin);
